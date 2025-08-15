675981c1bc2982a6361d03564f349d7360d48009
ShurGith <75266693+ShurGith@users.noreply.github.com>
2025-08-15 22:34:17 +0200

    viernes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
+import { corsConfig } from './config/cors';
 import { connectDB } from './config/db';
 import projectRoutes from './routes/projectRoutes';
 
@@ -8,6 +11,12 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(cors(corsConfig));
+//Loggin 
+app.use(morgan('dev'))
+
+//Leer datos de formularios.
 app.use(express.json());
 app.use('/api/projects', projectRoutes)
 
